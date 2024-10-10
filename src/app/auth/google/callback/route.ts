21d1af5cd3662f1dd19cb7675e1148b61af6823a
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2024-10-10 22:02:30 +0900

    feat: add logs

--- src/app/auth/google/callback/route.ts
+++ src/app/auth/google/callback/route.ts
@@ -7,17 +7,26 @@ import { COOKIE_NAME_GOOGLE_STATE, COOKIE_NAME_SESSION, bakeCookie } from "@web/
 export const dynamic = "force-dynamic";
 
 function redirect(req: NextRequest, err: string) {
-  const redirectUrl = new URL("/auth/login", req.nextUrl.origin);
-  redirectUrl.searchParams.set("error", err);
-  return Response.redirect(redirectUrl);
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: `/auth/login?error=${err}`,
+    },
+  });
 }
 
 export async function GET(req: NextRequest): Promise<Response> {
   const cookieState = req.cookies.get(COOKIE_NAME_GOOGLE_STATE);
-  if (cookieState == null) return redirect(req, "invalid_request");
+  if (cookieState == null) {
+    console.log("state cookie not found");
+    return redirect(req, "invalid_request");
+  }
 
   const claim = await verifyCallback(req.nextUrl, cookieState.value);
-  if (claim.isErr()) return redirect(req, "invalid_request");
+  if (claim.isErr()) {
+    console.log("failed to obtain id token claim", claim.error);
+    return redirect(req, "invalid_request");
+  }
 
   const session = await issueSessionWithGoogle(claim.value.sub);
 
