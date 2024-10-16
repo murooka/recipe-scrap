84df1e04540f5bb163cb451354bc24d0cebc3fb5
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2024-10-16 11:46:06 +0900

    feat: use RC version Next.js

--- src/app/authenticate.ts
+++ src/app/authenticate.ts
@@ -6,7 +6,7 @@ import type { User } from "@facade/types";
 import { COOKIE_NAME_SESSION } from "@web/cookie";
 
 export async function authenticate(): Promise<User> {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const sessionCookie = cookieStore.get(COOKIE_NAME_SESSION);
   if (sessionCookie == null) redirect("/auth/login");
 
