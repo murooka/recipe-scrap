13c086eac0549d0bec75735cbdf6f28983cc42d1
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2024-10-23 12:15:44 +0900

    feat: support file dnd

--- src/components/input-image.tsx
+++ src/components/input-image.tsx
@@ -1,14 +1,38 @@
-/* eslint-disable @next/next/no-img-element */
 import { RefreshCw } from "lucide-react";
 import { useCallback, useEffect, useRef, useState } from "react";
-import type { ChangeEvent, ComponentProps, ReactNode } from "react";
+import type { ChangeEvent, ComponentProps, DragEventHandler, ReactNode } from "react";
 
 import { Button } from "./button";
+import { GcpImage } from "./gcp-image";
+
+function useFileDrop(cb: (filelist: FileList) => void) {
+  const onDragOver: DragEventHandler<HTMLElement> = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+  const onDrop: DragEventHandler<HTMLElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (e.dataTransfer.files.length === 0) return;
+
+      cb(e.dataTransfer.files);
+    },
+    [cb],
+  );
+
+  return { onDragOver, onDrop } as const;
+}
 
 type InputImageProps = ComponentProps<"input"> & { defaultImageUrl?: string };
 export function InputImage(props: InputImageProps): ReactNode {
   const [file, setFile] = useState<File | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = useCallback((filelist: FileList) => {
+    setFile(filelist[0]);
+  }, []);
+
+  const buttonDropHandlers = useFileDrop(handleFileChange);
+  const imageDropHandlers = useFileDrop(handleFileChange);
 
   const onButtonClick = useCallback(() => {
     inputRef.current?.click();
@@ -16,9 +40,7 @@ export function InputImage(props: InputImageProps): ReactNode {
 
   const onFileInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
-    if (files && files.length > 0) {
-      setFile(files[0]);
-    }
+    if (files && files.length > 0) setFile(files[0]);
   }, []);
 
   const inputImageUrl = file ? URL.createObjectURL(file) : null;
@@ -35,7 +57,7 @@ export function InputImage(props: InputImageProps): ReactNode {
       <input ref={inputRef} {...props} type="file" accept="image/*" onChange={onFileInputChange} className="hidden" />
       {imageUrl ? (
         <div className="relative">
-          <img src={imageUrl} alt="" className="w-full" />
+          <GcpImage src={imageUrl} {...imageDropHandlers} alt="" className="w-full" />
           <button
             type="button"
             onClick={onButtonClick}
@@ -45,7 +67,7 @@ export function InputImage(props: InputImageProps): ReactNode {
           </button>
         </div>
       ) : (
-        <Button type="button" variant="outline" onClick={onButtonClick}>
+        <Button type="button" variant="outline" onClick={onButtonClick} {...buttonDropHandlers}>
           ファイルを選択
         </Button>
       )}
