e5560090046de122f3c69c2c06bc829ae382045e
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2024-11-11 17:31:23 +0900

    feat: add storage to service static files

--- terraform/recipe-scrap-prod/storage.tf
+++ terraform/recipe-scrap-prod/storage.tf
@@ -16,8 +16,23 @@ resource "google_storage_bucket" "user_assets" {
   force_destroy               = false
 }
 
-resource "google_storage_bucket_iam_binding" "storage_file_viewer" {
+resource "google_storage_bucket_iam_binding" "user_assets__storage_file_viewer" {
   bucket  = google_storage_bucket.user_assets.name
+  role    = google_project_iam_custom_role.storage_file_viewer.id
+  members = ["allUsers"]
+}
+
+resource "google_storage_bucket" "assets" {
+  name                        = "a.rscrap.site"
+  location                    = local.region
+  storage_class               = "STANDARD"
+  public_access_prevention    = "inherited"
+  uniform_bucket_level_access = true
+  force_destroy               = false
+}
+
+resource "google_storage_bucket_iam_binding" "assets__storage_file_viewer" {
+  bucket  = google_storage_bucket.assets.name
   role    = google_project_iam_custom_role.storage_file_viewer.id
   members = ["allUsers"]
 }
