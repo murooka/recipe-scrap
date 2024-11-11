5d261e486150cc950df2d8bfc35cc07b8d675fa5
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2024-11-11 17:53:18 +0900

    feat: service static files via GCS

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 // @ts-check
 
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  assetPrefix: isProd ? "https://a.rscrap.site" : undefined,
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
