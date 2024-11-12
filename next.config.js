292a6c52b131bb80457ddfacf15ac0df4cfeb296
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2024-11-12 10:23:02 +0900

    feat: add serverExternalPackages for Google Cloud and YouTube APIs

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "standalone",
   assetPrefix: isProd ? "https://a.rscrap.site" : undefined,
+  serverExternalPackages: [
+    "@google-cloud/storage",
+    "@google-cloud/vision",
+    "@googleapis/youtube",
+    "google-auth-library",
+  ],
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
