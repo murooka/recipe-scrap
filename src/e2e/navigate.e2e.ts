6795523638a82447f44b4a754ac23388f643dd5e
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2024-11-12 19:39:59 +0900

    test: fix e2e test

--- src/e2e/navigate.e2e.ts
+++ src/e2e/navigate.e2e.ts
@@ -1,10 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("should navigate to the foo page", async ({ page }) => {
+test("should navigate to the login page", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await expect(page.getByText("Go to foo")).toBeVisible();
-  await page.click("text=Go to foo");
 
-  await expect(page).toHaveURL("http://localhost:3000/foo");
-  await expect(page.getByText("This is foo")).toBeVisible();
+  await page.waitForURL("http://localhost:3000/auth/login");
+  await expect(page.getByText("Login with Google")).toBeVisible();
 });
