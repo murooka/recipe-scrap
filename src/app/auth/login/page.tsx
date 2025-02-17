04fcd056e803aa06fecfdc418f0ae15f86df7cfb
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2025-02-17 21:43:42 +0900

    feat: tweak login button design

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import type { ReactNode } from "react";
 
+import { Button } from "@components/button";
+
 export default function Page(): ReactNode {
   return (
-    <div>
-      <Link href="/auth/google/login">Login with Google</Link>
+    <div className="grid h-64 place-items-center">
+      <Button asChild>
+        <Link href="/auth/google/login">Login with Google</Link>
+      </Button>
     </div>
   );
 }
